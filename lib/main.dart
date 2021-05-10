3eeb28f00bb65ce75895cb4f88e6a902a0b3c417
FlutLab.io <>
2021-05-10 19:46:55 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -97,7 +97,7 @@ class Inicio extends StatelessWidget {
                         height: 100,
                         child: Center(
                           child: Text(
-                            "Inicio",
+                            "INICIO",
                             textAlign: TextAlign.center,
                             style: TextStyle(color: Colors.indigo, fontWeight: FontWeight.w600),
                           ), //Fin Center
@@ -128,8 +128,56 @@ class Inicio extends StatelessWidget {
                     )
                   ],
                 ),//r1 columna 1
+                Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.white,
+                        shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/productos");
+                        },
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Center(
+                            child: Text("PRODUCTOS", textAlign: TextAlign.center),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.white,
+                        shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/contacto");
+                        },
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Center(
+                            child: Text("CONTACTO", textAlign: TextAlign.center),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
             ]//Widget            
             ), //Fin  Fila 1
+            
           ], //Fin widget 1
         ), //Cierre Columna Child    
       ), //Fin Container
